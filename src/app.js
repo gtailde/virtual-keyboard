10fbf36bdc9551ddcb8fdaee0642e5b11917dc0e
gtailde <gtailde@gmail.com>
2023-04-27 21:49:23 +0200

    feat: added HTML markup to the drawElement function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
+import { drawElement } from "./drawElement.js";
+import { checkBtnClick } from "./checkBtnClick.js";
+import { addLetter } from "./addLetter.js";
+import { getTextContent } from "./getTextContent.js";
+
 class App {}
